1bf0e254a4e5ddd5f2d9033483c37368f91b89cc
strevecloud <strevecloud@gmail.com>
2018-02-09 11:25:47 +0700

    add new ip and upgradable framework

--- app/config.js
+++ app/config.js
@@ -1,3 +1,4 @@
 // export const URL = 'https://laravelfresh-sefriandsz.c9users.io/public/'
 // export const URL = 'http://173.212.252.213:14001/api/'
-export const URL = 'http://5.189.159.246:20001/api/'
+// export const URL = 'http://5.189.159.246:20001/api/'
+export const URL = 'http://207.148.78.72:10001/api/'
